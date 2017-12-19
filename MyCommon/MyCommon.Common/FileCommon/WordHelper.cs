510ff8a43e8c4dc9d395cc6dafc661fc907bdd2f
417827155 <417827155@qq.com>
2017-12-19 17:45:26 +0800

    新增文件

---
+++ MyCommon/MyCommon.Common/FileCommon/WordHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCommon.Common.FileCommon
+{
+    public class WordHelper
+    {
+    }
+}
