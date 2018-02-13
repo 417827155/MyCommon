07bf15ee06d7c545ba7e3345cb252943e6948034
417827155 <417827155@qq.com>
2018-02-13 17:06:19 +0800

    测试

--- MyCommon/MyCommon.Common/FileCommon/FileHelper.cs
+++ MyCommon/MyCommon.Common/FileCommon/FileHelper.cs
@@ -15,57 +15,8 @@ namespace MyCommon.Common.FileCommon
     {
         #region 路径
         /// <summary>
-        /// 根据给出的相对地址获取网站绝对地址
-        /// </summary>
-        /// <param name="localPath">相对地址</param>
-        /// <returns>绝对地址</returns>
-        public static string GetWebPath(string localPath)
-        {
-            string path = HttpContext.Current.Request.ApplicationPath;
-            string thisPath;
-            string thisLocalPath;
-            //如果不是根目录就加上"/" 根目录自己会加"/"
-            if (path != "/")
-            {
-                thisPath = path + "/";
-            }
-            else
-            {
-                thisPath = path;
-            }
-            if (localPath.StartsWith("~/"))
-            {
-                thisLocalPath = localPath.Substring(2);
-            }
-            else
-            {
-                return localPath;
-            }
-            return thisPath + thisLocalPath;
-        }
-
-        /// <summary>
-        ///  获取网站绝对地址
-        /// </summary>
-        /// <returns></returns>
-        public static string GetWebPath()
-        {
-            string path = HttpContext.Current.Request.ApplicationPath;
-            string thisPath;
-            //如果不是根目录就加上"/" 根目录自己会加"/"
-            if (path != "/")
-            {
-                thisPath = path + "/";
-            }
-            else
-            {
-                thisPath = path;
-            }
-            return thisPath;
-        }
-
-        /// <summary>
         /// 根据相对路径或绝对路径获取绝对路径
+        /// e.g.E:\张嵩\myself\MyCommon\MyCommon\MyCommon.Web\Home 
         /// </summary>
         /// <param name="localPath">相对路径或绝对路径</param>
         /// <returns>绝对路径</returns>
